97fbb2483cbed07a058c600cdfac008c6804dac4
dhilip <kdhilip146@gmail.com>
2025-04-17 14:57:55 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Dhilip from './dhilip'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Dhilip/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/dhilip.jsx
@@ -0,0 +1,9 @@
+function Dhilip() {
+
+    return(
+          <div className="ind">
+                dhilip
+                </div>
+    );
+}
+export default Dhilip;
